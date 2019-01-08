f5b3458b451e318fbc531479212d47a64823dbb7
alexreiling <alexanderreiling@googlemail.com>
2019-01-08 19:53:35 +0100

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+function main() {
+  var result = "Hello, World!"
+  
+  
+  
+  // die nächsten 2 Zeilen stehen lassen
+  var resultTag = document.getElementById("result")
+  resultTag.innerHTML = result
+}
